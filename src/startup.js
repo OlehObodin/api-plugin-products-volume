4341c5646fb8f45770f21b0db246ae2b85cb1cb1
Oleh <dev@it-lead.eu>
2022-09-06 14:24:38 +0200

    extend the favorite scheme

--- src/startup.js
+++ src/startup.js
@@ -1,17 +1,25 @@
 export function myStartup(context) {
-    context.simpleSchemas.ProductVariant.extend({
-      volume: {
-        type: Number,
-        min: 0,
-        optional: true
-      }
-    });
-  
-    context.simpleSchemas.CatalogProductVariant.extend({
-      volume: {
-        type: Number,
-        min: 0,
-        optional: true
-      }
-    })
-  }
+  context.simpleSchemas.ProductVariant.extend({
+    volume: {
+      type: Number,
+      min: 0,
+      optional: true,
+    },
+  });
+
+  context.simpleSchemas.CatalogProductVariant.extend({
+    volume: {
+      type: Number,
+      min: 0,
+      optional: true,
+    },
+  });
+
+  context.simpleSchemas.FavoriteProductVariant.extend({
+    volume: {
+      type: Number,
+      min: 0,
+      optional: true,
+    },
+  });
+}
